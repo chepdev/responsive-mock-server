e4c9a9eaa219248fe52c365f18e8add41549ac31
JP Erasmus <jperasmus11@gmail.com>
2017-02-08 14:42:19 +0200

    Update README 🤓

--- test/sample-responses/create-new-user-photo.js
+++ test/sample-responses/create-new-user-photo.js
@@ -1,5 +1,5 @@
 module.exports = function(req, res) {
-  const userId = req.query.userId;
+  const userId = req.param.userId;
   return {
     'status': 'OK',
     'message': `Nice one! Photo created successfully for user: ${userId}!`
